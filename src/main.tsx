709a42495b53af35dd1667cca3e6244ebf7ca1a0
Ela Myczkowska <emyczkow@poczta.fm>
2024-05-27 18:50:13 +0200

    Prepared structure representing the CV in the render function

--- src/main.tsx
+++ src/main.tsx
@@ -36,6 +36,35 @@ const cvData: CVData = {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <h1>Hej</h1>
+    <main>
+      <header>
+        <h1>CV - {cvData.name} {cvData.lastName}</h1>
+      </header>
+      <aside>
+        <h2>Personal data</h2>
+        <img src={cvData.photo} alt={`${cvData.name} ${cvData.lastName} profile photo`} />
+        <p>{ cvData.name } { cvData.lastName }</p>
+        <small>{ cvData.position }</small>
+      </aside>
+      <section>
+        <h2>Experience</h2>
+        <ul>
+          { cvData.experience.map((experience) => (
+            <li key={ experience.year }>
+              <strong>{ experience.year }</strong> - { experience.description }
+            </li>
+          ))}
+        </ul>
+        <h2>Education</h2>
+        <ul>
+          { cvData.education.map ((education, index) => (
+            <li key={ index }>
+              {education}
+            </li>
+          ))}
+        </ul>
+        <p><strong>Life motto:</strong> { cvData.lifeMotto }</p>
+      </section>
+    </main>
   </React.StrictMode>,
 )
